8ef23350a3ed12124e61a896e70358ed551c2f51
zelimran <elimrani.z@gmail.com>
2022-04-24 01:31:50 +0000

    Initializing project

---
+++ src/main/java/com/security/jwt/entity/User.java
@@ -0,0 +1,34 @@
+package com.security.jwt.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String email;
+    private String profileImageUrl;
+    private Date lastLoginDate;
+    private Date lastLoginDateDisplay;
+    private Date joinDate;
+    private String[] roles; // ROLE_USER, ROLE_ADMIN
+    private String[] authorities; // delete, update, create, read, edit
+    private boolean isActive;
+    private boolean isNotLocked;
+
+}
