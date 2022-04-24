f281611900dbf92eea9c6d299b22523c72c9feb8
zelimran <elimrani.z@gmail.com>
2022-04-24 02:49:53 +0000

    Customized Entry point errors ( Forbidden, Unauthorized )

---
+++ src/main/java/com/security/jwt/security/JwtAuthentificationEntryPoint.java
@@ -0,0 +1,30 @@
+package com.security.jwt.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.security.jwt.helpers.HttpResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+import org.springframework.util.MimeTypeUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class JwtAuthentificationEntryPoint extends Http403ForbiddenEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
+        HttpResponse httpResponse = new HttpResponse(HttpStatus.FORBIDDEN.value(), HttpStatus.FORBIDDEN, HttpStatus.FORBIDDEN.getReasonPhrase().toUpperCase(), SecurityConstant.FORBIDDEN_MESSAGE);
+
+        response.setContentType(MimeTypeUtils.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+
+        OutputStream outputStream = response.getOutputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(outputStream, httpResponse);
+        outputStream.flush();
+    }
+
+}
